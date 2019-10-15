9200bc6dc7b9d191a976e3fc0e61f891cad12ec4
Anthony Bruscantini <ajbruscantini@gmail.com>
2019-10-15 17:55:25 -0400

    handle short version strings

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 module.exports = function cmp (a, b) {
     var pa = a.split('.');
     var pb = b.split('.');
+    while (pa.length != 3) {
+      pa.push('0');
+    }
+    while (pb.length != 3) {
+      pb.push('0');
+    }
     for (var i = 0; i < 3; i++) {
         var na = Number(pa[i]);
         var nb = Number(pb[i]);
